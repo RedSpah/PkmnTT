146e1a311ccf200ab6114923212b9e1221d8dc19
redspah <redspah@gmail.com>
2020-06-08 00:27:22 +0100

    first

---
+++ PkmnTypeTester/PkmnTypeTester/Datatypes.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PkmnTypeTester
+{
+    static class Datatypes
+    {
+        // internal
+
+        static List<List<double>> type_matchups = new List<List<double>>();
+        static List<string> type_names = new List<string>();
+        static List<Typing> all_monotypings = new List<Typing>();
+        static List<Type> all_types = new List<Type>();
+        public static int type_count { get; private set; }
+
+        static List<ulong> bits = new List<ulong>();
+
+        public static List<Type> get_all_types()
+        {
+            return all_types;
+        }
+
+        public static double AttackCalc(Typing attacking, Typing defending)
+        {
+            double mult = 1.0d;
+            List<Type> atkt = attacking.Types();
+            List<Type> deft = defending.Types();
+
+            foreach (Type atk in atkt)
+            {
+                foreach (Type def in deft)
+                {
+                    mult *= type_matchups[atk.type_index][def.type_index];
+                }
+            }
+            return mult;
+        }
+
+        public static string TypeName(int index)
+        {
+            return type_names[index];
+        }
+
+        public static bool Init(string filepath)
+        {
+            if (!File.Exists(filepath))
+            {
+                return false;
+            }
+
+            List<string> chart_lines = File.ReadAllText(filepath).Split('\n').Select(x => x.Trim()).ToList();
+            type_names = chart_lines[0].Split(',').ToList();
+            type_count = type_names.Count();
+
+            chart_lines.Skip(1).ToList().ForEach(x =>
+            {
+                List<double> cur_matchups = new List<double>();
+                x.Split(',').ToList().ForEach(y =>
+                {
+                    cur_matchups.Add(Double.Parse(y, System.Globalization.NumberStyles.Float));
+                });
+                type_matchups.Add(cur_matchups);
+            });
+
+            ulong bit = 1;
+
+            for (int i = 0; i < type_count; i++)
+            {
+                bits.Add(bit);
+                bit *= 2;
+            }
+
+            for (int i = 0; i < type_count; i++)
+            {
+                all_types.Add(new Type(i));
+            }
+
+            for (int i = 0; i < type_count; i++)
+            {
+                all_monotypings.Add(new Typing(new Type(i)));
+            }
+
+           
+
+            Console.WriteLine("Initialized " + filepath);
+            return true;
+        }
+
+        public static List<Typing> Combinations(int num_types, bool incl_lesser = true)
+        {
+            List<Typing> all_steps = new List<Typing>(all_monotypings);
+
+            List<Typing> current = all_monotypings;
+
+            for (int i = 1; i < num_types; i++)
+            {
+                List<Typing> product = new List<Typing>();
+
+                foreach (Typing t1 in current)
+                {
+                    foreach (Type t2 in all_types)
+                    {
+                        if (t1.Contains(t2)) { continue; }
+                        Typing mix = t1.Product(t2);
+                        if (!product.Contains(mix)) { product.Add(mix); }
+                    }
+                }
+
+                all_steps = all_steps.Concat(product).ToList();
+                current = new List<Typing>(product);
+            }
+
+            if (incl_lesser)
+            {
+                return all_steps;
+            }
+            else
+            {
+                return current;
+            }
+        }
+
+
+
+
+        public struct Type
+        {
+            public readonly int type_index;
+
+            public Type(int index)
+            {
+                type_index = index;
+            }
+
+            public override string ToString()
+            {
+                return type_names[type_index];
+            }
+        }
+
+        public class Typing : IEquatable<Typing>
+        {
+            //List<Type> types;
+            public readonly ulong type_bitmask = 0;
+
+            public Typing(Type t1, params Type[] ts)
+            {
+                // types.Add(t1);
+                type_bitmask = bits[t1.type_index];
+                foreach (Type t in ts)
+                {
+                    if (!Contains(t))
+                    {
+                        // types.Add(t);
+                        type_bitmask += bits[t.type_index];
+                    }
+                }
+            }
+
+            public Typing(ulong bitmask)
+            {
+                type_bitmask = bitmask;
+            }
+
+            public List<Type> Types()
+            {
+                List<Type> types = new List<Type>();
+                foreach (Type t in all_types)
+                {
+                    if (Contains(t))
+                    {
+                        types.Add(t);
+                    }
+                }
+                return types;
+            }
+
+            //public List<Type>.Enumerator GetEnumerator()
+            //{
+            //    return types.GetEnumerator();
+            // }
+
+            public Typing Product(Typing other)
+            {
+                //return new Typing(types[0], types.Skip(1).Concat(other.types).ToArray());
+                return new Typing(type_bitmask | other.type_bitmask);
+            }
+
+            public Typing Product(Type other)
+            {
+                // return new Typing(other, types.ToArray());
+                return new Typing(type_bitmask | bits[other.type_index]);
+            }
+
+            public bool Contains(Type t)
+            {
+                return (type_bitmask & bits[t.type_index]) != 0;
+            }
+
+            //public bool Contains(Typing t)
+            //{
+            //    return (type_bitmask & bits[t.type_index]) != 0;
+            //}
+
+            public bool Equals(Typing other)
+            {
+                return type_bitmask == other.type_bitmask;
+            }
+
+            public override string ToString()
+            {
+                List<Type> types = Types();
+
+                string typestr = types[0].ToString();
+
+                for (int i = 1; i < types.Count(); i++)
+                {
+                    typestr += "/" + types[i].ToString();
+                }
+
+                return typestr;
+            }
+
+            // public long TypeProduct()
+            // {
+            //     return type_product;
+            // }
+        }
+
+        /*
+        public class Typing : IEquatable<Typing>
+        {
+            List<Type> types;
+            ulong type_bitmask = 0;
+
+            public Typing(Type t1, params Type[] ts)
+            {
+                types.Add(t1);
+                type_bitmask = bits[t1.type_index];
+                foreach (Type t in ts)
+                {
+                    if (!types.Contains(t))
+                    {
+                        types.Add(t);
+                        type_bitmask += bits[t.type_index];
+                    }
+                }
+            }
+
+            public List<Type>.Enumerator GetEnumerator()
+            {
+                return types.GetEnumerator();
+            }
+
+            public Typing Product(Typing other)
+            {
+                return new Typing(types[0], types.Skip(1).Concat(other.types).ToArray());
+            }
+
+            public Typing Product(Type other)
+            {
+                return new Typing(other, types.ToArray());
+            }
+
+            public bool Contains(Type t)
+            {
+                return (type_bitmask & bits[t.type_index]) != 0;
+            }
+
+            //public bool Contains(Typing t)
+            //{
+            //    return (type_bitmask & bits[t.type_index]) != 0;
+            //}
+
+            public bool Equals(Typing other)
+            {
+                return type_bitmask == other.type_bitmask;
+            }
+
+            public override string ToString()
+            {
+                string typestr = types[0].ToString();
+
+                for (int i = 1; i < types.Count(); i++)
+                {
+                    typestr += "/" + types[i].ToString();
+                }
+
+                return typestr;
+            }
+
+            public long TypeProduct()
+            {
+                return type_product;
+            }
+        }
+        */
+    }
+}
---
+++ PkmnTypeTester/PkmnTypeTester/Program.cs
@@ -0,0 +1,370 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static PkmnTypeTester.Datatypes;
+
+
+namespace PkmnTypeTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select type chart to be used: ");
+
+            while (!Datatypes.Init(Console.ReadLine()))
+            {
+                Console.WriteLine("File doesn't exist. Try again: ");
+            }
+
+            List<Typing> atk_types = Combinations(1);
+
+            List<TypeDefMatchup> matchups = new List<TypeDefMatchup>();
+            List<TypeAtkMatchup> atkmatchups = new List<TypeAtkMatchup>();
+
+            List<Typing> Combo2 = Combinations(2);
+
+            Combo2.ForEach(x => matchups.Add(new TypeDefMatchup(atk_types, x)));
+            Combinations(2, false).ForEach(x => atkmatchups.Add(new TypeAtkMatchup(x, Combo2)));
+
+            matchups.Sort((x, y) => -Math.Sign(x.score - y.score));
+            atkmatchups.Sort((x, y) => -Math.Sign(x.score - y.score));
+
+            int pos = 1;
+            int ctr = 0;
+            double cur_score = matchups[0].score;
+
+            matchups.ForEach(tm =>
+            {
+                ctr++;
+                if (cur_score > tm.score)
+                {
+                    pos = ctr;
+                    cur_score = tm.score;
+                }
+
+                string str = String.Format("{5,5}.) {0,18}:  Resists: {1,2} | Weaknesses: {2,2} | Immunes: {3,2} | Score: {4,5}", tm.def, tm.resists, tm.weaknesses, tm.immunities, tm.score, pos);
+
+                Console.WriteLine(str);
+            });
+
+            Console.WriteLine();
+
+            List<Stats> def_stats = new List<Stats>();
+
+            get_all_types().ForEach(t =>
+            {
+                List<double> scores = new List<double>();
+                matchups.Where(x => x.def.Contains(t)).ToList().ForEach(x => scores.Add(x.score));
+                scores.Sort();
+                double mean = scores.Average();
+                double median = 0;
+
+                if (scores.Count % 2 == 1)
+                {
+                    median = scores[(scores.Count - 1) / 2];
+                }
+                else
+                {
+                    median = (scores[scores.Count / 2] + scores[(scores.Count / 2) - 1]) / 2;
+                }
+
+                double max = scores.Max();
+                double min = scores.Min();
+                double range = max - min;
+                double stddev = Math.Sqrt(scores.Select(x => Math.Pow(x - mean, 2)).Sum() / (scores.Count - 1));
+                def_stats.Add(new Stats(t, min, max, mean, median, stddev));
+            
+            });
+
+            def_stats.Sort((x, y) => -Math.Sign(x.median - y.median));
+
+            def_stats.ForEach(x => Console.WriteLine(String.Format("{0,9}:   Min: {1,-5:0.##} | Max: {2,-5:0.##} | Mean: {3,-5:0.##} | Median: {4,-5:0.##} | Stddev: {5,-5:0.##}", x.type, x.min, x.max, x.mean, x.median, x.stddev)));
+
+
+            Console.WriteLine();
+
+            {
+                List<double> scores = new List<double>();
+                matchups.ForEach(x => scores.Add(x.score));
+                scores.Sort();
+                double mean = scores.Average();
+                double median = 0;
+
+                if (scores.Count % 2 == 1)
+                {
+                    median = scores[(scores.Count - 1) / 2];
+                }
+                else
+                {
+                    median = (scores[scores.Count / 2] + scores[(scores.Count / 2) - 1]) / 2;
+                }
+
+                double max = scores.Max();
+                double min = scores.Min();
+                double range = max - min;
+                double stddev = Math.Sqrt(scores.Select(x => Math.Pow(x - mean, 2)).Sum() / (scores.Count - 1));
+
+                string str = String.Format("{0,9}:   Min: {1,-5:0.##} | Max: {2,-5:0.##} | Mean: {3,-5:0.##} | Median: {4,-5:0.##} | Stddev: {5,-5:0.##}", "TOTAL", min, max, mean, median, stddev);
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine(); Console.WriteLine(); Console.WriteLine();
+
+            pos = 1;
+            ctr = 0;
+            cur_score = atkmatchups[0].score;
+
+            atkmatchups.ForEach(tm =>
+            {
+                ctr++;
+                if (cur_score > tm.score)
+                {
+                    pos = ctr;
+                    cur_score = tm.score;
+                }
+
+                string str = String.Format("{5,5}.) {0,18}:  Not Effectives: {1,3} | Super Effectives: {2,3} | Immunes: {3,3} | Score: {4,5}", tm.atk, tm.resists, tm.weaknesses, tm.immunities, tm.score, pos);
+
+                Console.WriteLine(str);
+            });
+
+            Console.WriteLine();
+
+            List<Stats> atk_stats = new List<Stats>();
+
+            get_all_types().ForEach(t =>
+            {
+                List<double> scores = new List<double>();
+                atkmatchups.Where(x => x.atk.Contains(t)).ToList().ForEach(x => scores.Add(x.score));
+                scores.Sort();
+                double mean = scores.Average();
+                double median = 0;
+
+                if (scores.Count % 2 == 1)
+                {
+                    median = scores[(scores.Count - 1) / 2];
+                }
+                else
+                {
+                    median = (scores[scores.Count / 2] + scores[(scores.Count / 2) - 1]) / 2;
+                }
+
+                double max = scores.Max();
+                double min = scores.Min();
+                double range = max - min;
+                double stddev = Math.Sqrt(scores.Select(x => Math.Pow(x - mean, 2)).Sum() / (scores.Count - 1));
+                atk_stats.Add(new Stats(t, min, max, mean, median, stddev));
+            });
+
+            atk_stats.Sort((x, y) => -Math.Sign(x.median - y.median));
+
+            atk_stats.ForEach(x => Console.WriteLine(String.Format("{0,9}:   Min: {1,-6:0.##} | Max: {2,-6:0.##} | Mean: {3,-6:0.##} | Median: {4,-6:0.##} | Stddev: {5,-6:0.##}", x.type, x.min, x.max, x.mean, x.median, x.stddev)));
+
+            Console.WriteLine();
+
+            {
+                List<double> scores = new List<double>();
+                atkmatchups.ForEach(x => scores.Add(x.score));
+                scores.Sort();
+                double mean = scores.Average();
+                double median = 0;
+
+                if (scores.Count % 2 == 1)
+                {
+                    median = scores[(scores.Count - 1) / 2];
+                }
+                else
+                {
+                    median = (scores[scores.Count / 2] + scores[(scores.Count / 2) - 1]) / 2;
+                }
+
+                double max = scores.Max();
+                double min = scores.Min();
+                double range = max - min;
+                double stddev = Math.Sqrt(scores.Select(x => Math.Pow(x - mean, 2)).Sum() / (scores.Count - 1));
+
+                string str = String.Format("{0,9}:   Min: {1,-6:0.##} | Max: {2,-6:0.##} | Mean: {3,-6:0.##} | Median: {4,-6:0.##} | Stddev: {5,-6:0.##}", "TOTAL", min, max, mean, median, stddev);
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+
+        class TypeDefMatchup
+        {
+            public readonly Typing def;
+            public readonly int immunities = 0;
+            public readonly int triple_resists = 0;
+            public readonly int double_resists = 0;
+            public readonly int single_resists = 0;
+            public readonly int resists = 0;
+            public readonly int neutrals = 0;
+            public readonly int weaknesses = 0;
+            public readonly int single_weaknesses = 0;
+            public readonly int double_weaknesses = 0;
+            public readonly int triple_weaknesses = 0;
+            public readonly float score = 0;
+
+            public TypeDefMatchup(List<Typing> atk, Typing def_)
+            {
+                def = def_;
+
+                foreach (Typing at in atk)
+                {
+                    double res = AttackCalc(at, def);
+                    if (Eq(res, 1))
+                    {
+                        neutrals++;
+                    }
+                    else if (Eq(res, 2))
+                    {
+                        weaknesses++;
+                        single_weaknesses++;
+                        score -= 1;
+                    }
+                    else if (Eq(res, 4))
+                    {
+                        weaknesses++;
+                        double_weaknesses++;
+                        score -= 1.5f;
+                    }
+                    else if (Eq(res, 8))
+                    {
+                        weaknesses++;
+                        triple_weaknesses++;
+                        score -= 1.75f;
+                    }
+                    else if (Eq(res, 1.0d / 2))
+                    {
+                        resists++;
+                        single_resists++;
+                        score += 1;
+                    }
+                    else if (Eq(res, 1.0d / 4))
+                    {
+                        resists++;
+                        double_resists++;
+                        score += 1.5f;
+                    }
+                    else if (Eq(res, 1.0d / 8))
+                    {
+                        resists++;
+                        triple_resists++;
+                        score += 1.75f;
+                    }
+                    else if (Eq(res, 0))
+                    {
+                        immunities++;
+                        score += 2;
+                    }
+                }
+            }
+        }
+
+        class TypeAtkMatchup
+        {
+            public readonly Typing atk;
+            public readonly int immunities = 0;
+            public readonly int triple_resists = 0;
+            public readonly int double_resists = 0;
+            public readonly int single_resists = 0;
+            public readonly int resists = 0;
+            public readonly int neutrals = 0;
+            public readonly int weaknesses = 0;
+            public readonly int single_weaknesses = 0;
+            public readonly int double_weaknesses = 0;
+            public readonly int triple_weaknesses = 0;
+            public readonly float score = 0;
+
+            public TypeAtkMatchup(Typing atk_, List<Typing> def)
+            {
+                atk = atk_;              
+
+                foreach (Typing d in def)
+                {
+                    List<double> resses = new List<double>();
+
+                    foreach (Datatypes.Type t in atk.Types())
+                    {
+                        Typing tp = new Typing(t);
+                        resses.Add(AttackCalc(tp, d));
+                    }
+
+
+                    double res = resses.Max();
+                    if (Eq(res, 1))
+                    {
+                        neutrals++;
+                    }
+                    else if (Eq(res, 2))
+                    {
+                        weaknesses++;
+                        single_weaknesses++;
+                        score += 1;
+                    }
+                    else if (Eq(res, 4))
+                    {
+                        weaknesses++;
+                        double_weaknesses++;
+                        score += 1.5f;
+                    }
+                    else if (Eq(res, 8))
+                    {
+                        weaknesses++;
+                        triple_weaknesses++;
+                        score += 1.75f;
+                    }
+                    else if (Eq(res, 1.0d / 2))
+                    {
+                        resists++;
+                        single_resists++;
+                        score += -1;
+                    }
+                    else if (Eq(res, 1.0d / 4))
+                    {
+                        resists++;
+                        double_resists++;
+                        score += -1.5f;
+                    }
+                    else if (Eq(res, 1.0d / 8))
+                    {
+                        resists++;
+                        triple_resists++;
+                        score += -1.75f;
+                    }
+                    else if (Eq(res, 0))
+                    {
+                        immunities++;
+                        score += -2;
+                    }
+                }
+            }
+        }
+
+        public struct Stats 
+        {
+            public Datatypes.Type type;
+            public double min, max, mean, median, stddev;
+            public Stats(Datatypes.Type t, double min_, double max_, double mean_, double median_, double stddev_)
+            {
+                type = t;
+                min = min_;
+                max = max_;
+                mean = mean_;
+                median = median_;
+                stddev = stddev_;
+            }
+        }
+
+        static bool Eq(double a, double b)
+        {
+            return Math.Abs(a - b) < 0.0001d;
+        }
+    }
+}
